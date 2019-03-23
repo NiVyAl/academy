15db53bfa32f9e9f0d16d5d3fe1402468bb301d6
nick <nikitavisoc@gmail.com>
2019-03-23 23:56:53 +0300

    начал меню

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -2,4 +2,20 @@ var advert = document.querySelector(".advert");
 
 var advertClose = function() {
   advert.style.display = "none";
+};
+
+var menuButton = document.querySelector(".menu-button");
+console.log('h1');
+var isMenuOpen = false;
+
+var menuOpen = function() {
+  if (isMenuOpen) {
+    menuButton.classList.remove('menu-button--close');
+    isMenuOpen = false;
+  }
+  else {
+    menuButton.classList.add('menu-button--close');
+    isMenuOpen = true;
+  }
+  
 }
