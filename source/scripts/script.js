c4753ea5f4e2c29c04c9248fe57800d2ad85650e
nick <nikitavisoc@gmail.com>
2019-03-25 23:41:54 +0300

    меню на мобильной версии

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -5,16 +5,18 @@ var advertClose = function() {
 };
 
 var menuButton = document.querySelector(".menu-button");
-console.log('h1');
+var menu = document.querySelector(".main-nav__container");
 var isMenuOpen = false;
 
 var menuOpen = function() {
   if (isMenuOpen) {
+    menu.classList.remove('main-nav__container--active');
     menuButton.classList.remove('menu-button--close');
     isMenuOpen = false;
   }
   else {
     menuButton.classList.add('menu-button--close');
+    menu.classList.add('main-nav__container--active');
     isMenuOpen = true;
   }
   
